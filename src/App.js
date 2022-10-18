242ca3f0fcd5a4b809b2496da2b1ed3e48402c0d
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-10-18 21:45:10 +0900

    some code cleanup of commented out / unused code

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import {
 import { BrowserRouter } from "react-router-dom";
 
 import "./App.css";
-import { About } from "./components/About";
-import { Banner } from "./components/Banner";
+import { About } from "./components/about/About";
+import { Banner } from "./components/banner/Banner";
 import { Contact } from "./components/contact/contact";
 import { Footer } from "./components/footer/Footer";
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/navbar/Navbar";
 import { Projects } from "./components/projects/Projects";
-import { Skills } from "./components/Skills";
+import { Skills } from "./components/skills/Skills";
 
 const theme = createTheme({
   palette: {
