975c4bb05345838516a8fa2075c4578cf69c0b45
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-10-19 22:30:05 +0900

    added SEO component for title and meta tags with Helmet

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   StyledEngineProvider,
   ThemeProvider,
 } from "@mui/material/styles";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 
 import "./App.css";
@@ -15,7 +16,10 @@ import { Contact } from "./components/contact/contact";
 import { Footer } from "./components/footer/Footer";
 import { Navbar } from "./components/navbar/Navbar";
 import { Projects } from "./components/projects/Projects";
+import SearchEngineOptimization from "./components/SEO/SEO";
 import { Skills } from "./components/skills/Skills";
+
+const helmetContext = {};
 
 const theme = createTheme({
   palette: {
@@ -56,19 +60,33 @@ const theme = createTheme({
 export default function App() {
   return (
     <BrowserRouter>
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-
-          <Navbar />
-          <Banner />
-          <About />
-          <Skills />
-          <Projects />
-          <Contact />
-          <Footer />
-        </ThemeProvider>
-      </StyledEngineProvider>
+      <HelmetProvider context={helmetContext}>
+        <StyledEngineProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {/* <Helmet>
+              <title>Kyle Jeffrey</title>
+              <meta
+                name="description"
+                content="Portfolio website for fullstack web developer Kyle Jeffrey"
+              />
+            </Helmet> */}
+            <SearchEngineOptimization
+              title="Kyle Jeffrey"
+              description="Portfolio website for fullstack web developer Kyle Jeffrey"
+              name="Kyle Jeffrey"
+              type="Portfolio"
+            />
+            <Navbar />
+            <Banner />
+            <About />
+            <Skills />
+            <Projects />
+            <Contact />
+            <Footer />
+          </ThemeProvider>
+        </StyledEngineProvider>
+      </HelmetProvider>
     </BrowserRouter>
   );
 }
