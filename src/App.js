3464553c270bfcbc167a9bce8b8fd954739e5d85
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-10-19 22:53:15 +0900

    uninstalled Helmet and removed all helmet code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {
   StyledEngineProvider,
   ThemeProvider,
 } from "@mui/material/styles";
-import { Helmet, HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 
 import "./App.css";
@@ -16,10 +15,7 @@ import { Contact } from "./components/contact/contact";
 import { Footer } from "./components/footer/Footer";
 import { Navbar } from "./components/navbar/Navbar";
 import { Projects } from "./components/projects/Projects";
-import SearchEngineOptimization from "./components/SEO/SEO";
 import { Skills } from "./components/skills/Skills";
-
-const helmetContext = {};
 
 const theme = createTheme({
   palette: {
@@ -60,33 +56,19 @@ const theme = createTheme({
 export default function App() {
   return (
     <BrowserRouter>
-      <HelmetProvider context={helmetContext}>
-        <StyledEngineProvider injectFirst>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            {/* <Helmet>
-              <title>Kyle Jeffrey</title>
-              <meta
-                name="description"
-                content="Portfolio website for fullstack web developer Kyle Jeffrey"
-              />
-            </Helmet> */}
-            <SearchEngineOptimization
-              title="Kyle Jeffrey"
-              description="Portfolio website for fullstack web developer Kyle Jeffrey"
-              name="Kyle Jeffrey"
-              type="Portfolio"
-            />
-            <Navbar />
-            <Banner />
-            <About />
-            <Skills />
-            <Projects />
-            <Contact />
-            <Footer />
-          </ThemeProvider>
-        </StyledEngineProvider>
-      </HelmetProvider>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+
+          <Navbar />
+          <Banner />
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+          <Footer />
+        </ThemeProvider>
+      </StyledEngineProvider>
     </BrowserRouter>
   );
 }
